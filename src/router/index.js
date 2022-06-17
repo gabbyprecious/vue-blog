a88d3a999a0f58557afd84989206c710c87a1230
slaxware11 <slaxware@gmail.com>
2022-06-18 02:12:02 +0700

    Update index.js
    Fixed cors

--- src/router/index.js
+++ src/router/index.js
@@ -36,7 +36,10 @@ const routes = [
 
 const router = new VueRouter({
   mode: "history",
-  //base: process.env.BASE_URL,
+  base: process.env.BASE_URL,
+  headers: {
+          // remove headers
+  },
   routes,
 });
 
